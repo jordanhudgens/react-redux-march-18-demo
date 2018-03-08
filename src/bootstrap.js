e9984f1eda999dd4d166cc91efc8d607b18794b8
jordanhudgens <jordan@devcamp.com>
2018-03-07 22:28:49 -0600

    installed lodash and created initial reducers for redux along with installing axios for api communication

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
@@ -14,7 +17,11 @@ import './style/main.scss';
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={PostsIndex} />
+        </div>
+      </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
