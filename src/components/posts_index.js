31ff2fc077c184fbbbc8e40988f416cc9c2fda79
jordanhudgens <jordan@devcamp.com>
2018-03-08 00:11:33 -0600

    Added lifecycle hook for the posts index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
-    render() {
-      return (
-        <div>
-          Posts Index
-        </div>
-      )
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    )
+  }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
