e52a2cefe7cf2aa70f1bd28ce4267d66b1c519f6
jordanhudgens <jordan@devcamp.com>
2018-03-20 01:43:30 -0500

    Implemented form validations

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-export default class PostsNew extends Component {
-  render() {
+class PostsNew extends Component {
+  renderField(field) {
     return (
-      <div>
-        Posts new
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <pre>{field.meta.error}</pre>
       </div>
     );
   }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter at least one category";
+  }
+  if (!values.content) {
+    errors.content = "Content is required";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
